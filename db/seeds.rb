008db9ab4aa7073f02d57b9f8c350f7cd9f6ea3f
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2022-08-04 10:05:24 -0700

    added API structure, seeds, CORS'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+cats = [
+    {
+        name: "Mishi",
+        age: 2,
+        enjoys: "Terrorizing the neighbourhood"
+    },
+    {
+        name: "Dexter",
+        age: 3,
+        enjoys: "Eating everything and throwing up later"
+    },
+    {
+        name: "Lucifer",
+        age: 5,
+        enjoys: "Doing nothing all day long"
+    },
+    {
+        name: "Lucious",
+        age: 15,
+        enjoys: "prowling after the ladycats"
+    }
+]
+
+cats.each do |attributes|
+    Cat.create(attributes)
+    p "creating cat #{attributes}"
+end
